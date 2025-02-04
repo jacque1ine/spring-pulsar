5bb3e438ef9efbf40825f22b5291e9a97d3b9ab0
jacque1ine <js3fung@uwaterloo.ca>
2025-02-04 12:17:36 -0500

    working pulsar manager

---
+++ src/main/java/org/jacque1ine/springpulsar/SpringPulsarApplication.java
@@ -0,0 +1,13 @@
+package org.jacque1ine.springpulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPulsarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPulsarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jacque1ine/springpulsar/SpringPulsarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jacque1ine.springpulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPulsarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
